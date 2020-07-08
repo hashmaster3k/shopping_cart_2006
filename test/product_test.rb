96b0ad02cb7429870346f02c795ccc9fbbd0a4f8
Hashim Gari <hashmaster3k@gmail.com>
2020-07-08 09:18:01 -0600

    Adds functionality for hoarding product

--- test/product_test.rb
+++ test/product_test.rb
@@ -36,4 +36,19 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_product_hoarded?
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+    assert_equal false, product.is_hoarded?
+  end
+
+  def test_hoard_product
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+    product.hoard
+
+    assert_equal true, product.is_hoarded?
+  end
+
 end
